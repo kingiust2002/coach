779941660261a8f01bb0def11903fc8a2773ed97
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 13:03:57 +0330

    feat: configure web exercise media services

--- lib/app/app_dependencies_web.dart
+++ lib/app/app_dependencies_web.dart
@@ -1,10 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../core/config/media_config.dart';
 import '../features/athletes/data/memory_athlete_repository.dart';
+import '../features/exercises/data/exercise_video_store.dart';
 import '../features/exercises/data/memory_exercise_repository.dart';
+import '../features/exercises/data/remote_exercise_media_catalog.dart';
 import 'app_dependencies.dart';
 
 Future<AppDependencies> createAppDependencies() async {
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
   return AppDependencies(
     athleteRepository: MemoryAthleteRepository(),
     exerciseRepository: MemoryExerciseRepository(),
+    exerciseMediaCatalog: RemoteExerciseMediaCatalog(
+      endpoint: MediaConfig.exerciseManifestUri,
+      preferences: preferences,
+    ),
+    exerciseVideoStore: createExerciseVideoStore(),
   );
 }
