65f6581b387f03d8ca357e4fe686736fd30b06a6
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 13:04:53 +0330

    feat: initialize exercise media controller services

--- lib/app/coach_app.dart
+++ lib/app/coach_app.dart
@@ -6,7 +6,9 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import '../features/athletes/data/athlete_repository.dart';
 import '../features/athletes/presentation/athletes_controller.dart';
 import '../features/dashboard/presentation/app_shell.dart';
+import '../features/exercises/data/exercise_media_catalog.dart';
 import '../features/exercises/data/exercise_repository.dart';
+import '../features/exercises/data/exercise_video_store_base.dart';
 import '../features/exercises/presentation/exercises_controller.dart';
 import 'theme/app_theme.dart';
 
@@ -14,11 +16,15 @@ class CoachApp extends StatefulWidget {
   const CoachApp({
     required this.athleteRepository,
     required this.exerciseRepository,
+    this.exerciseMediaCatalog,
+    this.exerciseVideoStore,
     super.key,
   });
 
   final AthleteRepository athleteRepository;
   final ExerciseRepository exerciseRepository;
+  final ExerciseMediaCatalog? exerciseMediaCatalog;
+  final ExerciseVideoStore? exerciseVideoStore;
 
   @override
   State<CoachApp> createState() => _CoachAppState();
@@ -32,7 +38,11 @@ class _CoachAppState extends State<CoachApp> {
   void initState() {
     super.initState();
     _athletesController = AthletesController(widget.athleteRepository);
-    _exercisesController = ExercisesController(widget.exerciseRepository);
+    _exercisesController = ExercisesController(
+      widget.exerciseRepository,
+      mediaCatalog: widget.exerciseMediaCatalog,
+      videoStore: widget.exerciseVideoStore,
+    );
     unawaited(_athletesController.load());
     unawaited(_exercisesController.load());
   }
