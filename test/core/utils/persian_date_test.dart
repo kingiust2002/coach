6eea25fe74cc1e490fe012da519748ba61eeb086
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 12:45:38 +0330

    test: cover Persian date conversion

---
+++ test/core/utils/persian_date_test.dart
@@ -0,0 +1,13 @@
+import 'package:coach_app/core/utils/persian_date.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shamsi_date/shamsi_date.dart';
+
+void main() {
+  test('formats Gregorian date as Persian Jalali date', () {
+    expect(PersianDate.format(DateTime.utc(2026, 3, 21)), '۱۴۰۵/۰۱/۰۱');
+  });
+
+  test('converts selected Jalali date back to UTC calendar date', () {
+    expect(PersianDate.toUtcDate(Jalali(1405, 1, 1)), DateTime.utc(2026, 3, 21));
+  });
+}
