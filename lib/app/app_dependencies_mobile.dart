516a8fb09fef3453e68347ef8d7abdbb69fd5fac
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 13:03:44 +0330

    feat: configure mobile exercise media services

--- lib/app/app_dependencies_mobile.dart
+++ lib/app/app_dependencies_mobile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../core/config/media_config.dart';
 import '../core/database/app_database.dart';
 import '../features/athletes/data/sqlite_athlete_repository.dart';
+import '../features/exercises/data/exercise_video_store.dart';
+import '../features/exercises/data/remote_exercise_media_catalog.dart';
 import '../features/exercises/data/sqlite_exercise_repository.dart';
 import 'app_dependencies.dart';
 
@@ -13,8 +17,14 @@ Future<AppDependencies> createAppDependencies() async {
 
   final AppDatabase database = AppDatabase.instance;
   await database.open();
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
   return AppDependencies(
     athleteRepository: SqliteAthleteRepository(database),
     exerciseRepository: SqliteExerciseRepository(database),
+    exerciseMediaCatalog: RemoteExerciseMediaCatalog(
+      endpoint: MediaConfig.exerciseManifestUri,
+      preferences: preferences,
+    ),
+    exerciseVideoStore: createExerciseVideoStore(),
   );
 }
