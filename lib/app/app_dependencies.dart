406bf34afd33b87d65535b4e3ba0f9430ea8e655
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 13:03:30 +0330

    feat: inject exercise media services

--- lib/app/app_dependencies.dart
+++ lib/app/app_dependencies.dart
@@ -1,12 +1,18 @@
 import '../features/athletes/data/athlete_repository.dart';
+import '../features/exercises/data/exercise_media_catalog.dart';
 import '../features/exercises/data/exercise_repository.dart';
+import '../features/exercises/data/exercise_video_store_base.dart';
 
 class AppDependencies {
   const AppDependencies({
     required this.athleteRepository,
     required this.exerciseRepository,
+    required this.exerciseMediaCatalog,
+    required this.exerciseVideoStore,
   });
 
   final AthleteRepository athleteRepository;
   final ExerciseRepository exerciseRepository;
+  final ExerciseMediaCatalog exerciseMediaCatalog;
+  final ExerciseVideoStore exerciseVideoStore;
 }
