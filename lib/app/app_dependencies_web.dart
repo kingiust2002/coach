7b570b82cf58b823dd8060709e743b9a64d08e73
kingiust2002 <kingiust2002@gmail.com>
2026-07-28 12:43:20 +0330

    feat: wire web exercise media preview services

--- lib/app/app_dependencies_web.dart
+++ lib/app/app_dependencies_web.dart
@@ -1,4 +1,6 @@
 import '../features/athletes/data/memory_athlete_repository.dart';
+import '../features/exercises/data/exercise_media_downloader_web.dart';
+import '../features/exercises/data/memory_exercise_media_repository.dart';
 import '../features/exercises/data/memory_exercise_repository.dart';
 import 'app_dependencies.dart';
 
@@ -6,5 +8,7 @@ Future<AppDependencies> createAppDependencies() async {
   return AppDependencies(
     athleteRepository: MemoryAthleteRepository(),
     exerciseRepository: MemoryExerciseRepository(),
+    exerciseMediaRepository: MemoryExerciseMediaRepository(),
+    exerciseMediaDownloader: createExerciseMediaDownloader(),
   );
 }
